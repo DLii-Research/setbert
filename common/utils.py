1d56740aa7406686c3657268176193d882e6efb6
David Ludwig <davidludwigii@gmail.com>
2022-04-30 21:24:24 +0000

    Utils

---
+++ common/utils.py
@@ -0,0 +1,14 @@
+import tensorflow.keras as keras
+from . core.custom_objects import custom_objects
+
+def static_vars(**kwargs):
+    def decorate(func):
+        for k in kwargs:
+            setattr(func, k, kwargs[k])
+        return func
+    return decorate
+
+def load_model(path, custom_objs):
+    objs = custom_objects()
+    objs.update(custom_objs)
+    return keras.models.load_model(path, objs)
