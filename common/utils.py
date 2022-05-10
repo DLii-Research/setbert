24066f7d8bc7acbcb0814147dd87cfd02a45804f
David Ludwig <davidludwigii@gmail.com>
2022-05-10 02:53:35 +0000

    Update common boolean terms in str_to_bool

--- common/utils.py
+++ common/utils.py
@@ -2,16 +2,16 @@ import tensorflow.keras as keras
 from . core.custom_objects import custom_objects
 
 def static_vars(**kwargs):
-    def decorate(func):
-        for k in kwargs:
-            setattr(func, k, kwargs[k])
-        return func
-    return decorate
+	def decorate(func):
+		for k in kwargs:
+			setattr(func, k, kwargs[k])
+		return func
+	return decorate
 
 def load_model(path, custom_objs={}):
-    objs = custom_objects()
-    objs.update(custom_objs)
-    return keras.models.load_model(path, objs)
+	objs = custom_objects()
+	objs.update(custom_objs)
+	return keras.models.load_model(path, objs)
 
 def str_to_bool(s):
-    return s.strip().lower() in {'1', 'true', 'y', 'yes'}
+	return s.strip().lower() in {'1', 't', 'true', 'y', 'yes'}
