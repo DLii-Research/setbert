c7f967874081136020bd5da48b028ea66c0310ba
David Ludwig <davidludwigii@gmail.com>
2022-07-03 10:58:03 +0000

    Enable DNAGAST models to encode sequences with a dynamic batch size

--- common/models/dnagast.py
+++ common/models/dnagast.py
@@ -3,32 +3,27 @@ import tensorflow.keras as keras
 
 from common.core.custom_objects import CustomObject
 from common.models.gan import Gan, ConditionalGan, VeeGan, ConditionalVeeGan
+from common.utils import subbatch_predict
 
 @CustomObject
 class DnaSampleGan(Gan):
-    def __init__(self, generator, discriminator, encoder, batch_size, subsample_size, encoder_batch_size=512):
+    def __init__(self, generator, discriminator, encoder, encoder_batch_size=512):
         super().__init__(generator, discriminator)
         self.encoder = encoder
         self.encoder.trainable = False
         self.encoder_batch_size = encoder_batch_size
 
-        # Can't obtain these dynamically in TF...
-        self.batch_size = batch_size
-        self.subsample_size = subsample_size
-
     def modify_data_for_input(self, data):
-        flat_data = tf.reshape(data, (self.batch_size*self.subsample_size, -1))
-        encoded = []
-        for i in range(0, self.batch_size*self.subsample_size, self.encoder_batch_size):
-            encoded.append(tf.stop_gradient(self.encoder(flat_data[i:i+self.encoder_batch_size])))
-        return tf.reshape(tf.concat(encoded, axis=0), (self.batch_size, self.subsample_size, -1))
+        batch_size = tf.shape(data)[0]
+        subsample_size = tf.shape(data)[1]
+        flat_data = tf.reshape(data, (batch_size*subsample_size, -1))
+        encoded = subbatch_predict(self.encoder, flat_data, subsample_size)
+        return tf.reshape(encoded, (batch_size, subsample_size, -1))
 
     def get_config(self):
         config = super().get_config()
         config.update({
             "encoder": self.encoder,
-            "batch_size": self.batch_size,
-            "subsample_size": self.subsample_size,
             "encoder_batch_size": self.encoder_batch_size
         })
         return config
@@ -36,58 +31,46 @@ class DnaSampleGan(Gan):
 
 @CustomObject
 class DnaSampleConditionalGan(ConditionalGan):
-    def __init__(self, generator, discriminator, encoder, batch_size, subsample_size, encoder_batch_size=512):
+    def __init__(self, generator, discriminator, encoder, encoder_batch_size=512):
         super().__init__(generator, discriminator)
         self.encoder = encoder
         self.encoder.trainable = False
         self.encoder_batch_size = encoder_batch_size
 
-        # Can't obtain these dynamically in TF...
-        self.batch_size = batch_size
-        self.subsample_size = subsample_size
-
     def modify_data_for_input(self, data):
-        flat_data = tf.reshape(data[0], (self.batch_size*self.subsample_size, -1))
-        encoded = []
-        for i in range(0, self.batch_size*self.subsample_size, self.encoder_batch_size):
-            encoded.append(tf.stop_gradient(self.encoder(flat_data[i:i+self.encoder_batch_size])))
-        return tf.reshape(tf.concat(encoded, axis=0), (self.batch_size, self.subsample_size, -1))
+        batch_size = tf.shape(data[0])[0]
+        subsample_size = tf.shape(data[0])[1]
+        flat_data = tf.reshape(data[0], (batch_size*subsample_size, -1))
+        encoded = subbatch_predict(self.encoder, flat_data, subsample_size)
+        return (tf.reshape(encoded, (batch_size, subsample_size, -1)), data[1])
 
     def get_config(self):
         config = super().get_config()
         config.update({
             "encoder": self.encoder,
-            "batch_size": self.batch_size,
-            "subsample_size": self.subsample_size,
             "encoder_batch_size": self.encoder_batch_size
         })
         return config
     
 @CustomObject
 class DnaSampleVeeGan(VeeGan):
-    def __init__(self, generator, discriminator, reconstructor, encoder, batch_size, subsample_size, encoder_batch_size=512):
+    def __init__(self, generator, discriminator, reconstructor, encoder, encoder_batch_size=512):
         super().__init__(generator, discriminator)
         self.encoder = encoder
         self.encoder.trainable = False
         self.encoder_batch_size = encoder_batch_size
 
-        # Can't obtain these dynamically in TF...
-        self.batch_size = batch_size
-        self.subsample_size = subsample_size
-
     def encode_data(self, data):
-        flat_data = tf.reshape(data, (self.batch_size*self.subsample_size, -1))
-        encoded = []
-        for i in range(0, self.batch_size*self.subsample_size, self.encoder_batch_size):
-            encoded.append(tf.stop_gradient(self.encoder(flat_data[i:i+self.encoder_batch_size])))
-        return tf.reshape(tf.concat(encoded, axis=0), (self.batch_size, self.subsample_size, -1))
+        batch_size = tf.shape(data)[0]
+        subsample_size = tf.shape(data)[1]
+        flat_data = tf.reshape(data, (batch_size*subsample_size, -1))
+        encoded = subbatch_predict(self.encoder, flat_data, subsample_size)
+        return tf.reshape(encoded, (batch_size, subsample_size, -1))
 
     def get_config(self):
         config = super().get_config()
         config.update({
             "encoder": self.encoder,
-            "batch_size": self.batch_size,
-            "subsample_size": self.subsample_size,
             "encoder_batch_size": self.encoder_batch_size
         })
         return config
@@ -95,29 +78,23 @@ class DnaSampleVeeGan(VeeGan):
 
 @CustomObject
 class DnaSampleConditionalVeeGan(ConditionalVeeGan):
-    def __init__(self, generator, discriminator, reconstructor, encoder, batch_size, subsample_size, encoder_batch_size=512):
+    def __init__(self, generator, discriminator, reconstructor, encoder, encoder_batch_size=512):
         super().__init__(generator, discriminator, reconstructor)
         self.encoder = encoder
         self.encoder.trainable = False
         self.encoder_batch_size = encoder_batch_size
 
-        # Can't obtain these dynamically in TF...
-        self.batch_size = batch_size
-        self.subsample_size = subsample_size        
-
     def encode_data(self, data):
-        flat_data = tf.reshape(data[0], (self.batch_size*self.subsample_size, -1))
-        encoded = []
-        for i in range(0, self.batch_size*self.subsample_size, self.encoder_batch_size):
-            encoded.append(tf.stop_gradient(self.encoder(flat_data[i:i+self.encoder_batch_size])))
-        return (tf.reshape(tf.concat(encoded, axis=0), (self.batch_size, self.subsample_size, -1)), data[1])
+        batch_size = tf.shape(data[0])[0]
+        subsample_size = tf.shape(data[0])[1]
+        flat_data = tf.reshape(data[0], (batch_size*subsample_size, -1))
+        encoded = subbatch_predict(self.encoder, flat_data, subsample_size)
+        return (tf.reshape(encoded, (batch_size, subsample_size, -1)), data[1])
 
     def get_config(self):
         config = super().get_config()
         config.update({
             "encoder": self.encoder,
-            "batch_size": self.batch_size,
-            "subsample_size": self.subsample_size,
             "encoder_batch_size": self.encoder_batch_size
         })
         return config
--- common/utils.py
+++ common/utils.py
@@ -30,7 +30,27 @@ def accumulate(a, b):
         return [accumulate(x, y) for x, y in zip(a, b)]
     return a + b
 
-def subbatch_train_step(train_step, batch, subbatch_size, models, optimizers, return_gradients=False):
+def subbatch_predict(model, batch, subbatch_size, concat=lambda old, new: tf.concat((old, new), axis=0)):
+    def predict(i, result=None):
+        n = i + subbatch_size
+        pred = tf.stop_gradient(model(batch[i:n]))
+        if result is None:
+            return [n, pred]
+        return [n, concat(result, pred)]
+    
+    # First pass to obtain initial results
+    i, result = predict(0)
+    
+    batch_size = tf.shape(batch)[0]
+    i, result = tf.while_loop(
+        cond=lambda i, _: i < batch_size,
+        body=predict,
+        loop_vars=[i, result],
+        parallel_iterations=1)
+    
+    return result
+
+def accumulate_train_step(train_step, batch, subbatch_size, models):
     """
     A generic implementation of accumulating/gradients via sub-batching.
     
@@ -51,7 +71,6 @@ def subbatch_train_step(train_step, batch, subbatch_size, models, optimizers, re
     
     if type(models) not in (list, tuple):
         models = (models,)
-        optimizers = (optimizers,)
     
     # First pass to obtain variables/gradients
     accum_grads = [[tf.zeros_like(w) for w in model.trainable_weights] for model in models]
@@ -65,47 +84,4 @@ def subbatch_train_step(train_step, batch, subbatch_size, models, optimizers, re
         loop_vars=[i, variables, accum_grads],
         parallel_iterations=1)
     
-    # Apply the gradients for each model
-    for model, optimizer, grads in zip(models, optimizers, accum_grads):
-        optimizer.apply_gradients(zip(grads, model.trainable_weights))
-    if return_gradients:
-        return variables, accum_grads
-    return variables
-
-# def subbatch_train_step(train_step, batch, subbatch_size, variables, models, optimizers):
-#     """
-#     A generic implementation of accumulating/gradients via sub-batching.
-    
-#     train_step should return a list of variables, followed by a list of gradients
-#     """
-#     def subbatch_forward_pass(i, *args):
-#         n = i + subbatch_size
-#         subbatch = (batch[0][i:n], batch[1][i:n])
-        
-#         # Perform training step
-#         variables, grads = train_step(subbatch, *args)
-        
-#         # Accumulate gradients
-#         accum_grads = args[len(variables):]
-#         accum_grads = [[(g + ag) for g, ag in zip(gs, ags)] for gs, ags in zip(grads, accum_grads)]
-        
-#         return [n, variables, accum_grads]
-    
-#     # Setup loop variables
-#     batch_size = tf.shape(batch[0])[0]
-#     i = tf.constant(0, dtype=tf.int32)
-    
-#     # Initialize the accumulated gradients
-#     accum_grads = [[tf.zeros_like(w) for w in model.trainable_weights] for model in models]
-    
-#     # Iterate over each sub-batch
-#     i, variables, accum_grads = tf.while_loop(
-#         cond=lambda i, *_: i < batch_size,
-#         body=subbatch_forward_pass,
-#         loop_vars=[i, *variables, *accum_grads],
-#         parallel_iterations=1)
-    
-#     # Apply the gradients for each model
-#     for model, optimizer, grads in zip(models, optimizers, accum_grads):
-#         optimizer.apply_gradients(zip(grads, model.trainable.weights))
-#     return variables
+    return variables, accum_grads
