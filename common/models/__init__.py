6fcc3b1ce36ee032550a0cb2b7849a4b9c12a442
David Ludwig <davidludwigii@gmail.com>
2022-07-03 08:48:01 +0000

    Add sub-batching utilities. Default training regime supports sub-batching through the CustomModel

--- common/models/__init__.py
+++ common/models/__init__.py
@@ -1,8 +1,14 @@
+import tensorflow as tf
 import tensorflow.keras as keras
 from .. core.custom_objects import CustomObject
-from .. utils import load_model
+from .. utils import load_model, subbatch_train_step
 
 class CustomModel(keras.Model):
+    
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.subbatching = True # temporary for testing/debugging
+        self.__subbatch_size = tf.Variable(0, dtype=tf.int32, trainable=False, name="Sub_batch_size")
 
     @classmethod
     def from_config(cls, config):
@@ -14,3 +20,30 @@ class CustomModel(keras.Model):
     @staticmethod
     def load(*args, **kwargs):
         return load_model(*args, **kwargs)
+    
+    def train_step(self, batch):
+        """
+        The standard training regime supporting accumulating gradients for large batch training.
+        """
+        if not self.subbatching:
+            return super().train_step(batch)
+        def step(batch):
+            x, y = batch
+            with tf.GradientTape() as tape:
+                y_pred = self(x, training=True)
+                loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+            grads = tape.gradient(loss, self.trainable_weights)
+            self.compiled_metrics.update_state(y, y_pred)
+            return [], [grads]
+        subbatch_train_step(step, batch, self.subbatch_size, self, self.optimizer)
+        return {m.name: m.result() for m in self.metrics}
+    
+    def fit(self, *args, subbatch_size=None, **kwargs):
+        if subbatch_size is None or subbatch_size <= 0:
+            subbatch_size = 2**31 - 1 # max int32
+        self.__subbatch_size.assign(subbatch_size)
+        return super().fit(*args, **kwargs)
+    
+    @property
+    def subbatch_size(self):
+        return self.__subbatch_size
--- common/utils.py
+++ common/utils.py
@@ -1,5 +1,6 @@
 import io
 from PIL import Image
+import tensorflow as tf
 import tensorflow.keras as keras
 from . core.custom_objects import custom_objects as registered_custom_objects
 
@@ -23,3 +24,88 @@ def plt_to_image(plt, format="png"):
     plt.savefig(buf, format=format)
     buf.seek(0)
     return Image.open(buf)
+
+def accumulate(a, b):
+    if type(a) in (list, tuple):
+        return [accumulate(x, y) for x, y in zip(a, b)]
+    return a + b
+
+def subbatch_train_step(train_step, batch, subbatch_size, models, optimizers, return_gradients=False):
+    """
+    A generic implementation of accumulating/gradients via sub-batching.
+    
+    train_step should return a list of variables, followed by a list of gradients
+    """
+    def step(i, accum_variables=None, accum_grads=None):
+        n = i + subbatch_size
+        subbatch = (batch[0][i:n], batch[1][i:n])
+        
+        # Perform training step
+        variables, grads = train_step(subbatch)
+        
+        # Accumulate variables and gradients
+        if accum_variables is not None:
+            variables = accumulate(variables, accum_variables)
+        grads = [[(g + ag) for g, ag in zip(gs, ags)] for gs, ags in zip(grads, accum_grads)]    
+        return [n, variables, grads]
+    
+    if type(models) not in (list, tuple):
+        models = (models,)
+        optimizers = (optimizers,)
+    
+    # First pass to obtain variables/gradients
+    accum_grads = [[tf.zeros_like(w) for w in model.trainable_weights] for model in models]
+    i, variables, accum_grads = step(0, None, accum_grads)
+    
+    # Loop for any additional updates
+    batch_size = tf.shape(batch[0])[0]
+    i, variables, accum_grads = tf.while_loop(
+        cond=lambda i, *_: i < batch_size,
+        body=step,
+        loop_vars=[i, variables, accum_grads],
+        parallel_iterations=1)
+    
+    # Apply the gradients for each model
+    for model, optimizer, grads in zip(models, optimizers, accum_grads):
+        optimizer.apply_gradients(zip(grads, model.trainable_weights))
+    if return_gradients:
+        return variables, accum_grads
+    return variables
+
+# def subbatch_train_step(train_step, batch, subbatch_size, variables, models, optimizers):
+#     """
+#     A generic implementation of accumulating/gradients via sub-batching.
+    
+#     train_step should return a list of variables, followed by a list of gradients
+#     """
+#     def subbatch_forward_pass(i, *args):
+#         n = i + subbatch_size
+#         subbatch = (batch[0][i:n], batch[1][i:n])
+        
+#         # Perform training step
+#         variables, grads = train_step(subbatch, *args)
+        
+#         # Accumulate gradients
+#         accum_grads = args[len(variables):]
+#         accum_grads = [[(g + ag) for g, ag in zip(gs, ags)] for gs, ags in zip(grads, accum_grads)]
+        
+#         return [n, variables, accum_grads]
+    
+#     # Setup loop variables
+#     batch_size = tf.shape(batch[0])[0]
+#     i = tf.constant(0, dtype=tf.int32)
+    
+#     # Initialize the accumulated gradients
+#     accum_grads = [[tf.zeros_like(w) for w in model.trainable_weights] for model in models]
+    
+#     # Iterate over each sub-batch
+#     i, variables, accum_grads = tf.while_loop(
+#         cond=lambda i, *_: i < batch_size,
+#         body=subbatch_forward_pass,
+#         loop_vars=[i, *variables, *accum_grads],
+#         parallel_iterations=1)
+    
+#     # Apply the gradients for each model
+#     for model, optimizer, grads in zip(models, optimizers, accum_grads):
+#         optimizer.apply_gradients(zip(grads, model.trainable.weights))
+#     return variables
