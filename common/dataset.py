7887a3ec126197511bf828af047adbb60b0007f5
David Ludwig <davidludwigii@gmail.com>
2023-04-03 05:00:18 +0000

    Ensure dataset searches for compressed files.

--- common/dataset.py
+++ common/dataset.py
@@ -1,7 +1,7 @@
 import enum
 from pathlib import Path
 import re
-from typing import Callable, Generator
+from typing import Callable, Generator, Iterable
 
 class Split(enum.Flag):
     Train = enum.auto()
@@ -49,32 +49,33 @@ class Dataset:
             assert self.test_path is not None, "Dataset does not have a test split."
             yield from sorted((f for f in self.test_path.iterdir() if test(f)), key=numeric_prefix)
 
-    def find_with_suffix(self, suffix: str, split: Split) -> Generator[Path, None, None]:
+    def find_with_suffix(self, suffixes: Iterable[str], split: Split) -> Generator[Path, None, None]:
         """
         Find all files in a directory that end with the given suffex.
         """
-        yield from self.find(lambda x: x.name.endswith(suffix), split)
+
+        yield from self.find(lambda x: any(x.name.endswith(suffix) for suffix in suffixes), split)
 
     def fastas(self, split: Split) -> Generator[Path, None, None]:
         """
         Find all FASTA files in a directory.
         """
-        yield from self.find_with_suffix(".fasta", split)
+        yield from self.find_with_suffix([".fasta", ".fasta.gz"], split)
 
     def fasta_dbs(self, split: Split) -> Generator[Path, None, None]:
         """
         Find all FASTA db files in a directory.
         """
-        yield from self.find_with_suffix(".fasta.db", split)
+        yield from self.find_with_suffix([".fasta.db"], split)
 
     def taxonomies(self, split: Split) -> Generator[Path, None, None]:
         """
         Find all taxonomy files in a directory.
         """
-        yield from self.find_with_suffix("_taxonomy.tsv", split)
+        yield from self.find_with_suffix(["_taxonomy.tsv", "_taxonomy.tsv.gz"], split)
 
     def taxonomy_dbs(self, split: Split) -> Generator[Path, None, None]:
         """
         Find all taxonomy db files in a directory.
         """
-        yield from self.find_with_suffix("_taxonomy.tsv.db", split)
+        yield from self.find_with_suffix(["_taxonomy.tsv.db"], split)
