cafd75b8c84f656f713fdb7c366fd555ebc91c9e
David Ludwig <davidludwigii@gmail.com>
2023-09-30 14:35:12 +0000

    Changee taxonomy eval scripts to handle searching for FASTAs internally

--- scripts/taxonomy_eval_qiime.py
+++ scripts/taxonomy_eval_qiime.py
@@ -1,19 +1,43 @@
 import argparse
 import joblib
 from pathlib import Path
-import sys
-import time
+from sklearn.pipeline import Pipeline
 from tqdm.contrib.concurrent import process_map
 
-config = None
-model = None
+config: argparse.Namespace
+model: Pipeline
 
 def define_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("output_path", type=Path)
-    parser.add_argument("--qiime-classifier-path", type=Path, required=True)
-    parser.add_argument("--workers", type=int, default=1)
+
+    group = parser.add_argument_group("Dataset")
+    group.add_argument("--synthetic-data-path", type=Path, required=True)
+    group.add_argument("--dataset", type=str, required=True)
+    group.add_argument("--synthetic-classifier", type=str, required=True)
+    group.add_argument("--distribution", type=str, required=True, choices=["uniform", "natural"])
+
+    group = parser.add_argument_group("Job")
+    group.add_argument("--output-path", type=Path, required=True)
+    group.add_argument("--qiime-classifier-path", type=Path, required=True)
+    group.add_argument("--workers", type=int, default=1)
+
     return parser
+
+
+def find_fastas_to_process(
+    synthetic_data_path: Path,
+    dataset: str,
+    synthetic_classifier: str,
+    distribution: str,
+    output_path
+):
+    path = synthetic_data_path / dataset / synthetic_classifier
+    if distribution == "uniform":
+        path /= "test-uniform"
+    else:
+        path /= "test"
+    existing = set([f.name for f in output_path.iterdir() if f.name.endswith(".tax.tsv")])
+    return set([f for f in path.iterdir() if f.name.endswith(".fasta") and f.with_suffix(".tax.tsv").name not in existing])
 
 
 def read_fasta(path: Path):
@@ -45,11 +69,25 @@ def run(fasta_path):
 def main():
     global config
     global model
+
+    Path(config.output_path).mkdir(exist_ok=True)
+    output_path = config.output_path / config.dataset / config.synthetic_classifier
+    output_path.mkdir(exist_ok=True, parents=True)
+    fastas = find_fastas_to_process(
+        config.synthetic_data_path,
+        config.dataset,
+        config.synthetic_classifier,
+        config.distribution,
+        output_path)
+
+    if len(fastas) == 0:
+        print("No FASTA files to process.")
+        return
+
     # Extract the tar first...
     model = joblib.load(config.qiime_classifier_path / "sklearn_pipeline.pkl")
-    print("Writing to:", config.output_path)
-
-    fastas = list(map(Path, map(str.strip, sys.stdin.readlines())))
+    print("Writing to:", output_path)
+    config.output_path = output_path
     process_map(run, fastas, max_workers=config.workers, chunksize=1)
 
 if __name__ == "__main__":
--- scripts/taxonomy_eval_setbert.py
+++ scripts/taxonomy_eval_setbert.py
@@ -1,10 +1,7 @@
-import argparse
 from dnadb import dna
 import deepctx.scripting as dcs
 import numpy as np
 from pathlib import Path
-import sys
-import time
 from tqdm.auto import tqdm
 
 from deepdna.nn.models import load_model, taxonomy
@@ -13,12 +10,35 @@ from deepdna.nn.models import load_model, taxonomy
 def define_arguments(context: dcs.Context):
     parser = context.argument_parser
 
-    parser.add_argument("output_path", type=Path)
-    parser.add_argument("--chunk-size", type=int, default=None)
-    parser.add_argument("--single-sequence", action="store_true", default=False, help="If using a set-based model, only provide one sequence at a time.")
+    group = parser.add_argument_group("Dataset")
+    group.add_argument("--synthetic-data-path", type=Path, required=True)
+    group.add_argument("--dataset", type=str, required=True)
+    group.add_argument("--synthetic-classifier", type=str, required=True)
+    group.add_argument("--distribution", type=str, required=True, choices=["uniform", "natural"])
+
+    group = parser.add_argument_group("Job")
+    group.add_argument("--output-path", type=Path, required=True)
+    group.add_argument("--chunk-size", type=int, default=None)
+    group.add_argument("--single-sequence", action="store_true", default=False, help="Only provide one sequence at a time.")
 
     wandb = context.get(dcs.module.Wandb)
     wandb.add_artifact_argument("model", required=True, description="The deep-learning model to use.")
+
+
+def find_fastas_to_process(
+    synthetic_data_path: Path,
+    dataset: str,
+    synthetic_classifier: str,
+    distribution: str,
+    output_path
+):
+    path = synthetic_data_path / dataset / synthetic_classifier
+    if distribution == "uniform":
+        path /= "test-uniform"
+    else:
+        path /= "test"
+    existing = set([f.name for f in output_path.iterdir() if f.name.endswith(".tax.tsv")])
+    return set([f for f in path.iterdir() if f.name.endswith(".fasta") and f.with_suffix(".tax.tsv").name not in existing])
 
 
 def read_fasta(path: Path):
@@ -39,6 +59,21 @@ def write_tax_tsv(path: Path, entries):
 
 def main(context: dcs.Context):
     config = context.config
+
+    Path(config.output_path).mkdir(exist_ok=True)
+    output_path = config.output_path / config.dataset / config.synthetic_classifier
+    output_path.mkdir(exist_ok=True, parents=True)
+    fastas = find_fastas_to_process(
+        config.synthetic_data_path,
+        config.dataset,
+        config.synthetic_classifier,
+        config.distribution,
+        output_path)
+
+    if len(fastas) == 0:
+        print("No FASTA files to process.")
+        return
+
     wandb = context.get(dcs.module.Wandb)
 
     path = wandb.artifact_argument_path("model")
@@ -55,8 +90,7 @@ def main(context: dcs.Context):
 
     kmer = model.base.base.dnabert_encoder.base.kmer
 
-    for fasta_path in tqdm(sys.stdin.readlines()):
-        fasta_path = Path(fasta_path.strip())
+    for fasta_path in tqdm(fastas):
         ids, sequences = zip(*read_fasta(fasta_path))
         sequences = list(map(dna.encode_sequence, sequences))
         sequences = dna.encode_kmers(np.array(sequences), kmer)
@@ -65,7 +99,7 @@ def main(context: dcs.Context):
             # Swap so it's 1,000 subsamples each containing 1 sequence
             sequences = np.transpose(sequences, (1, 0, 2))
         labels = model.classify(sequences, batch_size=batch_size, verbose=0).flatten()
-        tax_tsv_path = (config.output_path / fasta_path.name).with_suffix(".tax.tsv")
+        tax_tsv_path = (output_path / fasta_path.name).with_suffix(".tax.tsv")
         write_tax_tsv(tax_tsv_path, zip(ids, labels))
 
 
