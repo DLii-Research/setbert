42644a8d3c8970db959bfc118cc9f6f4c75f2eeb
David Ludwig <davidludwigii@gmail.com>
2023-10-16 05:21:55 +0000

    New environment configuration settings and script

--- env.sh
+++ env.sh
@@ -1,23 +1,7 @@
-# Synthetic Data Directory
-synthetic_data_path=~/work/Datasets/Synthetic
+# Load the .env file
+set -o allexport
+source .env set
+set +o allexport
 
-# Models -------------------------------------------------------------------------------------------
-
-# DNABERT
-export dnabert_pretrain_silva=sirdavidludwig/model-registry/dnabert-pretrain-silva-64d-150l:v0
-export dnabert_taxonomy_naive=sirdavidludwig/dnabert-taxonomy/dnabert-taxonomy-naive-64d-150l:v0
-export dnabert_taxonomy_bertax=sirdavidludwig/dnabert-taxonomy/dnabert-taxonomy-bertax-64d-150l:v0
-export dnabert_taxonomy_topdown=sirdavidludwig/dnabert-taxonomy/dnabert-taxonomy-topdown-64d-150l:v0
-
-# DNABERT (deeper)
-export dnabert_taxonomy_topdown_deep=sirdavidludwig/dnabert-taxonomy/dnabert-taxonomy-topdown-deep-64d-150l:v0
-
-# SetBERT
-export setbert_pretrain_silva=sirdavidludwig/model-registry/setbert-pretrain-silva-64d-150l:v0
-export setbert_taxonomy_topdown=sirdavidludwig/model-registry/setbert-taxonomy-topdown-64d-150l:v0
-
-# SetBERT (leave-one-out controls)
-export setbert_taxonomy_topdown_nhs=sirdavidludwig/setbert-taxonomy/setbert-taxonomy-topdown-nhs-64d-150l:v0
-export setbert_taxonomy_topdown_nhw=sirdavidludwig/setbert-taxonomy/setbert-taxonomy-topdown-nhw-64d-150l:v0
-export setbert_taxonomy_topdown_nsw=sirdavidludwig/setbert-taxonomy/setbert-taxonomy-topdown-nsw-64d-150l:v0
-export setbert_taxonomy_topdown_hsw=sirdavidludwig/setbert-taxonomy/setbert-taxonomy-topdown-hsw-64d-150l:v0
+alias python_tf="$(echo "${python_prefix} ${python_tf}" | xargs)"
+alias python_q2="$(echo "${python_prefix} conda run -n ${qiime2_env} python3" | xargs)"
