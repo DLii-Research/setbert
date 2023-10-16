a8cb8f411f298daf1a17968eaed7720059e39049
David Ludwig <davidludwigii@gmail.com>
2023-10-16 21:00:50 +0000

    DNABERT Naive Taxonomy script

---
+++ jobs/dnabert_finetune_taxonomy_naive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --signal=INT@600
+
+source "$(dirname -- "$( readlink -f -- "$0"; )")/../env.sh"
+
+${python_prefix} ${python_tf} ./scripts/finetuning/dnabert_finetune_taxonomy_naive.py \
+    --wandb-name dnabert-taxonomy-naive-silva-64d-150l \
+    --wandb-project taxonomy-classification \
+    --dnabert-pretrain-artifact $dnabert_pretrain_silva \
+    --sequences-fasta-db $datasets_path/Synthetic/Synthetic.fasta.db \
+    --taxonomy-tsv-db $datasets_path/Synthetic/Synthetic.tax.tsv.db \
+    $@
