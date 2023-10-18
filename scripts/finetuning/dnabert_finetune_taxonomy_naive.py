30f3d89d445390b1433a2a60e31735da4fc230ac
David Ludwig <davidludwigii@gmail.com>
2023-10-18 04:45:08 +0000

    Make finetuning scripts resumeable

--- scripts/finetuning/dnabert_finetune_taxonomy_naive.py
+++ scripts/finetuning/dnabert_finetune_taxonomy_naive.py
@@ -136,6 +136,6 @@ if __name__ == "__main__":
             steps_per_epoch=100,
             val_steps_per_epoch=20)
     context.use(dcs.module.Rng)
-    context.use(dcs.module.Wandb)
+    context.use(dcs.module.Wandb).resumeable()
     define_arguments(context)
     context.execute()
