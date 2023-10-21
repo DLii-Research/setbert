965e40be1f8b5f3f004042d048434a5ab4d71794
David Ludwig <davidludwigii@gmail.com>
2023-10-21 06:40:27 +0000

    SFD only jobs

--- jobs/setbert_finetune_sfd_only_classifier_128d_250l.sh
+++ jobs/setbert_finetune_sfd_only_classifier_128d_250l.sh
@@ -4,7 +4,7 @@
 source "$(dirname -- "$( readlink -f -- "$0"; )")/../env.sh"
 
 ${python_prefix} ${python_tf} ./scripts/finetuning/setbert_finetune_sfd_binary_classification.py \
-    --wandb-name setbert-sfd-binary-classification-128d-250l \
+    --wandb-name setbert-sfd-only-classifier-128d-250l \
     --wandb-project sfd \
     --setbert-pretrain-artifact $setbert_pretrain_sfd_128d_250l \
     --sfd-dataset-path $datasets_path/SFD250 \
--- jobs/setbert_finetune_sfd_only_classifier_shallow_128d_250l.sh
+++ jobs/setbert_finetune_sfd_only_classifier_shallow_128d_250l.sh
@@ -4,9 +4,10 @@
 source "$(dirname -- "$( readlink -f -- "$0"; )")/../env.sh"
 
 ${python_prefix} ${python_tf} ./scripts/finetuning/setbert_finetune_sfd_binary_classification.py \
-    --wandb-name setbert-sfd-binary-classification-64d-150l \
+    --wandb-name setbert-sfd-only-classifier-128d-250l \
     --wandb-project sfd \
-    --setbert-pretrain-artifact $setbert_pretrain_sfd \
-    --sfd-dataset-path $datasets_path/SFD \
+    --setbert-pretrain-artifact $setbert_pretrain_sfd_128d_250l \
+    --sfd-dataset-path $datasets_path/SFD250 \
     --subsample-size 1000 \
+    --freeze-sequence-embeddings \
     $@
