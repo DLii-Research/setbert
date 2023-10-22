7a215fbd51eda8810424baa1dfea0870f9cfe649
David Ludwig <davidludwigii@gmail.com>
2023-10-22 16:56:41 +0000

    SetBERT pretrain script for real data

---
+++ jobs/setbert_pretrain_64d_150l.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --signal=INT@600
+
+source "$(dirname -- "$( readlink -f -- "$0"; )")/../env.sh"
+
+${python_prefix} ${python_tf} ./scripts/pretraining/setbert_pretrain.py \
+    --wandb-name setbert-pretrain-64d-150l \
+    --wandb-project setbert-sfd-pretrain \
+    --dnabert-pretrain-artifact $dnabert_pretrain_silva \
+    --datasets-path $datasets_path \
+    --datasets Nachusa,Hopland,SFD,Wetland \
+    $@
