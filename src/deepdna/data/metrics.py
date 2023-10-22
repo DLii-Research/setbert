7fa7f702e7fdb003a06acb13eedf8927040cbc3c
David Ludwig <davidludwigii@gmail.com>
2023-10-22 16:55:46 +0000

    Add ppv_npv_curve metric

--- src/deepdna/data/metrics.py
+++ src/deepdna/data/metrics.py
@@ -2,6 +2,42 @@ import multiprocessing
 import numpy as np
 from sklearn.manifold import MDS
 
+def binary_clf_curve(y_true, y_pred):
+    indices = np.argsort(y_pred)[::-1]
+    thresholds = y_pred[indices]
+    tps = np.cumsum(y_true[indices])
+    fps = np.cumsum(1 - y_true[indices])
+    return fps.astype(np.float64), tps.astype(np.float64), thresholds
+
+def ppv_npv_curve(y_true, y_pred, pad: bool = False):
+    if pad:
+        if not np.isclose(y_pred[0], 1.0):
+            y_true = np.concatenate(([1], y_true))
+            y_pred = np.concatenate(([1.0], y_pred))
+        if not np.isclose(y_pred[-1], 0.0):
+            y_true = np.concatenate((y_true, [0]))
+            y_pred = np.concatenate((y_pred, [0.0]))
+
+    fps, tps, thresholds = binary_clf_curve(y_true, y_pred)
+    fns, tns, _ = binary_clf_curve(1 - y_true, 1.0 - y_pred)
+
+    fns, tns = fns[::-1], tns[::-1]
+
+    ps = tps + fps
+    ns = tns + fns
+
+    # Initialize the result array with zeros to make sure that precision[ps == 0]
+    # does not contain uninitialized values.
+    ppv = np.zeros_like(tps)
+    np.divide(tps, ps, out=ppv, where=(ps != 0))
+
+    npv = np.zeros_like(tns)
+    np.divide(tns, ns, out=npv, where=(ps != 0))
+
+    # reverse the outputs so recall is decreasing
+    sl = slice(None, None, -1)
+    return np.hstack((ppv[sl], 1)), np.hstack((npv[sl], 0)), thresholds[sl]
+
 # Metric Multidimensional-scaling (MDS) ------------------------------------------------------------
 
 def mds(dist_mat, ndim=2, metric=True, seed=None, n_jobs=None, **kwargs):
