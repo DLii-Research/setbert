3bce4dc373891d9af761d3b4f62c8f8b2e4b6a55
David Ludwig <davidludwigii@gmail.com>
2023-11-08 09:15:27 +0000

    Add missing quotes

--- jobs/pretrain/dnabert/pretrain_dnabert_silva_nr99_filtered_515f_806r.sh
+++ jobs/pretrain/dnabert/pretrain_dnabert_silva_nr99_filtered_515f_806r.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=DNABERT Pre-train (SILVA NR99 Filtered 515F/806R)
+#SBATCH --job-name="DNABERT Pre-train (SILVA NR99 Filtered 515F/806R)"
 #SBATCH --signal=INT@600
 
 if [ -z "${deepdna_env_loaded}" ]; then
