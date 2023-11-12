ef386c4d4ac4b70deb17406e5d9fd428448430f4
David Ludwig <davidludwigii@gmail.com>
2023-11-12 16:15:11 +0000

    Change environment management structure

--- env.sh
+++ env.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 set -o allexport
-source .env set
-set +o allexport
 
-export deepdna_env_loaded=1
+# Load default environment variables
+source .env.example
+
+# Load environment variables from .env if it exists
+[ -f .env ] && source .env set
+
+# Load models
+source models.env
+
+# Finish loading
+set +o allexport
 
 alias python_tf="$(echo "${python_prefix} ${python_tf}" | xargs)"
 alias python_q2="$(echo "${python_prefix} conda run -n ${qiime2_env} python3" | xargs)"
+
+export deepdna_env_loaded=1
