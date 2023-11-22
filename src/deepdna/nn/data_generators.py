3e8ee7a889d9641359cdaf36ba2b749d48fa1055
David Ludwig <davidludwigii@gmail.com>
2023-11-22 00:18:05 +0000

    New data generators

--- src/deepdna/nn/data_generators.py
+++ src/deepdna/nn/data_generators.py
@@ -78,12 +78,33 @@ class BatchGenerator(tf.keras.utils.Sequence, Generic[IOType]):
         return self.batches_per_epoch
 
 
-def random_fasta_samples(
-    samples: Iterable[sample.FastaSample|sample.DemultiplexedFastaSample],
+def random_samples(
+    samples: sample.ISample|Iterable[sample.ISample],
+    shape: Optional[int|tuple[int, ...]] = None,
     weights: npt.NDArray[np.float_]|Literal["sample_size"]|None = None,
 ):
+    """
+    Draw random samples from the given list of samples with replacement.
+
+    Outputs:
+        samples: The samples
+    """
+    if isinstance(samples, sample.ISample):
+        samples = [samples]
+    if shape is None:
+        shape = ()
+    elif isinstance(shape, int):
+        shape = (shape,)
+
+    # Number of samples per batch element
+    n = np.product(shape or 1)
+
     # Convert to numpy array
-    samples = ndarray_from_iterable(samples)
+    samples = ndarray_from_iterable(list(samples))
+
+    # If only one sample, no need for random selection
+    if len(samples) == 1:
+        return lambda batch_size: dict(samples=np.repeat(samples, batch_size*n).reshape((batch_size, *shape)))
 
     # Compute weights
     if isinstance(weights, str) and weights == "sample_size":
@@ -91,33 +112,47 @@ def random_fasta_samples(
         p /= p.sum()
     else:
         p = weights
-    def factory(batch_size: int, np_rng: np.random.Generator):
-        return dict(samples=np_rng.choice(samples, size=batch_size, replace=True, p=p))
-    return factory
+    return lambda batch_size, np_rng: dict(samples=np_rng.choice(samples, size=(batch_size, *shape), replace=True, p=p))
 
 
 def random_sequence_entries(
     subsample_size: int|tuple[int, int]|None = None,
 ):
+    """
+    Retrieve random sequence entries from the available samples.
+
+    Inputs:
+        samples: list of samples
+    Outputs:
+        sequence_entries: The sequence entries
+    """
     if isinstance(subsample_size, tuple) and subsample_size[0] == subsample_size[1]:
         subsample_size = subsample_size[0]
     if isinstance(subsample_size, tuple):
         def factory(samples: np.ndarray, np_rng: np.random.Generator):
             return dict(
                 sequence_entries=recursive_map(
-                    lambda s: tuple(s.sample(np_rng.integers(*subsample_size), rng=np_rng)),
+                    lambda s: s.sample(np_rng.integers(*subsample_size), rng=np_rng),
                     samples))
     else:
         if subsample_size is None:
-            return lambda samples, np_rng: dict(sequence_entries=recursive_map(lambda s: next(s.sample(1, rng=np_rng)), samples))
+            return lambda samples, np_rng: dict(sequence_entries=recursive_map(lambda s: s.sample(1, rng=np_rng)[0], samples))
         else:
-            return lambda samples, np_rng: dict(sequence_entries=recursive_map(lambda s: tuple(s.sample(subsample_size, rng=np_rng)), samples))
+            return lambda samples, np_rng: dict(sequence_entries=recursive_map(lambda s: s.sample(subsample_size, rng=np_rng), samples))
     return factory
 
 
 def sequences(
     length: Optional[int|tuple[int, int]] = None
 ):
+    """
+    Retrieve the DNA sequence strings from the available sequence entries.
+
+    Inputs:
+        sequence_entries: list of sequence entries
+    Outputs:
+        sequences: The DNA sequences
+    """
     if length is None:
         return lambda sequence_entries: dict(sequences=recursive_map(lambda s: s.sequence, sequence_entries))
     def trim(sequence: str, length: int, np_rng: np.random.Generator):
@@ -142,17 +177,43 @@ def sequences(
     return factory
 
 
-def augment_ambiguous_bases(sequences, np_rng: np.random.Generator):
-    replacer = lambda match: np_rng.choice(dna.IUPAC_MAP[match[0]])
-    return dict(
-        sequences=recursive_map(lambda s: re.sub(f"[{dna.AMBIGUOUS_BASES}]", replacer, s), sequences))
+def augment_ambiguous_bases():
+    """
+    Augment the ambiguous bases in the DNA sequences.
+
+    Inputs:
+        sequences: The DNA sequences
+    Outputs:
+        sequences: The augmented DNA sequences
+    """
+    def factory(sequences, np_rng: np.random.Generator):
+        replacer = lambda match: np_rng.choice(dna.IUPAC_MAP[match[0]])
+        return dict(
+            sequences=recursive_map(lambda s: re.sub(f"[{dna.AMBIGUOUS_BASES}]", replacer, s), sequences))
+    return factory
 
 
 def pad_sequences(length: int, char: str = '_'):
+    """
+    Pad the DNA sequences to the given length.
+
+    Inputs:
+        sequences: The DNA sequences
+    Outputs:
+        sequences: The padded DNA sequences
+    """
     return lambda sequences: dict(sequences=recursive_map(lambda s: s + char*(length - len(s)), sequences))
 
 
 def encode_sequences(kmer: int = 1, ambiguous_bases: bool = False):
+    """
+    Encode the DNA sequence bases as integers
+
+    Inputs:
+        sequences: The DNA sequences
+    Outputs:
+        encoded_sequences: The encoded sequences
+    """
     if kmer == 1:
         return lambda sequences: dict(encoded_sequences=recursive_map(dna.encode_sequence, sequences))
     return lambda sequences: dict(
@@ -162,20 +223,77 @@ def encode_sequences(kmer: int = 1, ambiguous_bases: bool = False):
 
 
 def encode_kmers(kmer: int, augment_ambiguous_bases: bool = False):
+    """
+    Encode the sequences as k-mers.
+
+    Inputs:
+        encoded_sequences: The encoded sequences
+    Outputs:
+        encoded_kmer_sequences: The encoded k-mer sequences
+    """
     if kmer == 1:
         return lambda encoded_sequences: dict(encoded_kmer_sequences=np.array(encoded_sequences))
     return lambda encoded_sequences: dict(encoded_kmer_sequences=dna.encode_kmers(np.array(encoded_sequences), kmer, augment_ambiguous_bases))
 
 
-def taxonomy_indices(taxonomy_db: taxonomy.TaxonomyDb):
-    return lambda sequence_entries: dict(
-        taxonomy_indices=recursive_map(
-            lambda entry: taxonomy_db.fasta_id_to_index(entry.identifier),
-            sequence_entries))
+def random_taxonomy_entries(taxonomy_db: taxonomy.TaxonomyDb, shape: Optional[int|tuple[int, ...]] = None):
+    """
+    Retrieve random taxonomy entries from the given taxonomy database.
+
+    Outputs:
+        taxonomy_entries: The taxonomy entries
+    """
+    if shape is None:
+        shape = ()
+    elif isinstance(shape, int):
+        shape = (shape,)
+    return lambda batch_size, np_rng: dict(
+        taxonomy_entries=taxonomy_db.sample((batch_size, *shape), np_rng))
 
 
-def taxonomy_labels(taxonomy_db: taxonomy.TaxonomyDb):
-    return lambda sequence_entries: dict(
-        taxonomy_labels=recursive_map(
-            lambda entry: taxonomy_db.fasta_id_to_label(entry.identifier),
-            sequence_entries))
+def sequence_entries_from_taxonomy():
+    """
+    Retrieve the FASTA sequence entries from the available taxonomy entries.
+
+    Inputs:
+        taxonomy_entries: list of taxonomy entries
+    Outputs:
+        sequence_entries: The FASTA sequence entries
+    """
+    return lambda taxonomy_entries: dict(sequence_entries=np.array(recursive_map(lambda e: e.fasta_entry, taxonomy_entries)))
+
+
+def taxon_ids():
+    """
+    Extract taxon integer IDs from the available taxonomy entries.
+
+    Inputs:
+        taxonomy_entries: list of taxonomy entries
+    Outputs:
+        taxon_ids: The taxon IDs
+    """
+    return lambda taxonomy_entries: dict(taxon_ids=np.array(recursive_map(lambda e: e.taxonomy.taxon_ids, taxonomy_entries)))
+
+
+def taxonomy_id():
+    """
+    Extract the deepest taxonomy integer ID from the available taxonomy entries.
+
+    Inputs:
+        taxonomy_entries: list of taxonomy entries
+    Outputs:
+        taxonomy_id: The taxonomy ID
+    """
+    return lambda taxonomy_entries: dict(taxonomy_id=np.array(recursive_map(lambda e: e.taxonomy.taxonomy_id, taxonomy_entries)))
+
+
+def taxonomy_ids():
+    """
+    Extracts the taxonomy integer IDs for each depth from the available taxonomy entries.
+
+    Inputs:
+        taxonomy_entries: list of taxonomy entries
+    Outputs:
+        taxonomy_ids: list of taxonomy IDs
+    """
+    return lambda taxonomy_entries: dict(taxonomy_ids=np.array(recursive_map(lambda e: e.taxonomy.taxonomy_ids, taxonomy_entries)))
