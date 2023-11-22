d1b9b21f13ae2d7537dd86857d55bf11a36c5f5f
David Ludwig <davidludwigii@gmail.com>
2023-11-22 04:24:39 +0000

    Fix naive finetuning job

--- jobs/tasks/taxonomy/finetuning/dnabert/naive.sh
+++ jobs/tasks/taxonomy/finetuning/dnabert/naive.sh
@@ -2,4 +2,4 @@
 #SBATCH --job-name="DNABERT Taxonomy (Naive, $1)"
 #SBATCH --signal=INT@600
 
-./jobs/tasks/taxonomy/finetuning/dnabert/_finetune.sh bertax $@
+./jobs/tasks/taxonomy/finetuning/dnabert/_finetune.sh naive $@
