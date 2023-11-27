f6cbbdc0ad9d0a4c3d25856e4a3d63589730d4a7
David Ludwig <davidludwigii@gmail.com>
2023-11-27 23:23:21 +0000

    Add redundant SILVA

--- jobs/data/prepare_datasets.sh
+++ jobs/data/prepare_datasets.sh
@@ -7,15 +7,19 @@ mkdir -p ${datasets_path}
 function create_silva_dataset() {
     local dataset_path=$1
     local suffix=$2
+    local variant=$3
     if [ ! -z "${suffix}" ]; then
         suffix="-${suffix}"
+    fi
+    if [ ! -z "${variant}" ]; then
+        variant="-${variant}"
     fi
     mkdir -p "${dataset_path}"
     # Sequences
     if [ ! -d "${dataset_path}/sequences.fasta.db" ]; then
         dnadb fasta import \
             --min-length ${min_sequence_length} \
-            "${data_path}/silva/silva-${silva_version}-99-seqs${suffix}.fasta" \
+            "${data_path}/silva/silva-${silva_version}${variant}-seqs${suffix}.fasta" \
             "${dataset_path}/sequences.fasta.db"
     fi
     # Taxonomy
@@ -23,7 +27,7 @@ function create_silva_dataset() {
         dnadb taxonomy import \
             --depth ${taxonomy_precision} \
             --fasta-db "${dataset_path}/sequences.fasta.db" \
-            "${data_path}/silva/silva-${silva_version}-99-tax${suffix}.tsv" \
+            "${data_path}/silva/silva-${silva_version}${variant}-tax${suffix}.tsv" \
             "${dataset_path}/taxonomy.tax.db"
     fi
     # Test dataset
@@ -37,17 +41,29 @@ function create_silva_dataset() {
     fi
 }
 
+# SILVA
+echo "Preparing SILVA Dataset..."
+create_silva_dataset "${datasets_path}/silva" ""
+
 # SILVA NR99
 echo "Preparing SILVA NR99 Dataset..."
-create_silva_dataset "${datasets_path}/silva_nr99" ""
+create_silva_dataset "${datasets_path}/silva_nr99" "" "99"
+
+# SILVA Filtered
+echo "Preparing SILVA Filtered Dataset..."
+create_silva_dataset "${datasets_path}/silva_filtered" "derep-uniq"
 
 # SILVA NR99 Filtered
 echo "Preparing SILVA NR99 Filtered Dataset..."
-create_silva_dataset "${datasets_path}/silva_nr99_filtered" "derep-uniq"
+create_silva_dataset "${datasets_path}/silva_nr99_filtered" "derep-uniq" "99"
+
+# SILVA 515f/806r
+echo "Preparing SILVA 515f/806r Dataset..."
+create_silva_dataset "${datasets_path}/silva_515f_806r" "515f-806r-derep-uniq"
 
 # SILVA NR99 515F/806R
 echo "Preparing SILVA NR99 Filtered 515f/806r Dataset..."
-create_silva_dataset "${datasets_path}/silva_nr99_filtered_515f_806r" "515f-806r-derep-uniq"
+create_silva_dataset "${datasets_path}/silva_nr99_filtered_515f_806r" "515f-806r-derep-uniq" "99"
 
 # Hopland
 # mkdir -p "${datasets_path}/hopland"
--- vars.sh
+++ vars.sh
@@ -1,5 +1,8 @@
 # Silva Datasets
 declare -A datasets=(
+    [silva]=silva
+    [silva-filtered]=silva_filtered
+    [silva-filtered-515f-806r]=silva_filtered_515f_806r
     [silva-nr99]=silva_nr99
     [silva-nr99-filtered]=silva_nr99_filtered
     [silva-nr99-filtered-515f-806r]=silva_nr99_filtered_515f_806r
@@ -7,6 +10,9 @@ declare -A datasets=(
 
 # DNABERT
 declare -A dnabert_pretrain_artifacts=(
+    [silva]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva
+    [silva-filtered]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva-filtered
+    [silva-filtered-515f-806r]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva-filtered-515f-806r
     [silva-nr99]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva-nr99
     [silva-nr99-filtered]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva-nr99-filtered
     [silva-nr99-filtered-515f-806r]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva-nr99-filtered-515f-806r
