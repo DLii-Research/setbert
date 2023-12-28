1f8e0a15d1ab82bc6417fbac8728a87c02eebf55
David Ludwig <davidludwigii@gmail.com>
2023-12-28 10:29:12 -0600

    Update deep model reference

--- vars.sh
+++ vars.sh
@@ -21,7 +21,7 @@ declare -A dnabert_pretrain_artifacts=(
     [silva-nr99]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva-nr99
     [silva-nr99-filtered]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva-nr99-filtered
     [silva-nr99-filtered-515f-806r]=sirdavidludwig/model-registry/dnabert-pretrain-64d-150bp:silva-nr99-filtered-515f-806r
-    [deep-silva-nr99-filtered-515f-806r]=sirdavidludwig/dnabert-pretrain/dnabert-pretrain-deep-silva-nr99-filtered-515f-806r-64d-150bp:v0
+    [deep-silva-nr99-filtered-515f-806r]=sirdavidludwig/dnabert-pretrain/dnabert-pretrain-deep-silva-nr99-filtered-515f-806r-64d-150bp:v1
 )
 
 # DNABERT Taxonomy
