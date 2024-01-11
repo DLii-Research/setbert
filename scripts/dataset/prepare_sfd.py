398a91656d5a0a04b8f0de0513231689e85453c2
David Ludwig <davidludwigii@gmail.com>
2024-01-11 09:46:44 -0600

    Update SFD and Wetland dataset scripts

--- scripts/dataset/prepare_sfd.py
+++ scripts/dataset/prepare_sfd.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 from dataclasses import replace
-from dnadb import dna, fasta, sample, taxonomy
+from dnadb import dna, fasta, taxonomy
 import deepctx.scripting as dcs
 import pandas as pd
 import re
@@ -26,67 +26,77 @@ def main(context: dcs.Context):
     taxonomy_path = config.input_path / "230428_SFDtaxfinal_engineer.csv"
 
     # Create FASTA DB
-    factory = fasta.FastaDbFactory(output_path / f"{config.name}.fasta.db")
+    factory = fasta.FastaDbFactory(output_path / "sequences.fasta.db")
     for entry in tqdm(map(clean_entry, fasta.entries(fasta_path)), desc="Writing FASTA DB"):
         if len(entry) < config.min_length:
             continue
         factory.write_entry(entry)
     factory.close()
-    fasta_db = fasta.FastaDb(output_path / f"{config.name}.fasta.db")
+    fasta_db = fasta.FastaDb(output_path / "sequences.fasta.db")
 
     # Create FASTA Index DB
     with open(otu_list_path) as f:
         keys = f.readline().strip().split('\t')
         values = f.readline().strip().split('\t')
-    otu_index = dict(zip(keys[2:], values[2:]))
-    factory = fasta.FastaIndexDbFactory(output_path / f"{config.name}.fasta.index.db")
-    for _, asv in enumerate(tqdm(otu_index, desc="Writing FASTA Index DB")):
-        fasta_id = otu_index[asv]
-        if fasta_id not in fasta_db:
-            continue
-        factory.write_entry(fasta_db[fasta_id], key=asv)
-    factory.close()
-    index_db = fasta.FastaIndexDb(output_path / f"{config.name}.fasta.index.db")
+    otu_to_sequence_id = dict(zip(keys[2:], values[2:]))
 
     # Create sample mappings
     with open(otu_shared_path) as f:
         header = f.readline().strip().split('\t')
-        lines = [line.strip().split('\t') for line in tqdm(f)]
-    indices = [i for i in tqdm(range(3, len(header))) if index_db.contains_key(header[i])]
-    fasta_ids = {i: index_db.key_to_fasta_id(header[i]) for i in tqdm(indices)}
-    factory = sample.SampleMappingDbFactory(output_path / f"{config.name}.fasta.mapping.db")
-    for row in tqdm(lines, desc="Writing sample mappings"):
-        sample_name = row[1]
-        sample_factory = sample.SampleMappingEntryFactory(sample_name, index_db)
-        for i in indices:
-            if (count := int(row[i])) == 0:
-                continue
-            fasta_id = fasta_ids[i]
-            sample_factory.add_entry(fasta_db[fasta_id], count)
-        factory.write_entry(sample_factory.build())
-    factory.close()
 
-    # Taxonomy
-    taxonomy_data = pd.read_csv(taxonomy_path)
-    taxonomy_map = {}
-    for _, row in taxonomy_data.iterrows():
-        otu, *taxons = tuple(row)
-        label = taxonomy.join_taxonomy(taxons)
-        taxonomy_map[otu] = label
-    factory = taxonomy.TaxonomyDbFactory(output_path / f"{config.name}.tax.tsv.db")
-    for entry in tqdm(fasta_db, desc="Writing taxonomy"):
-        otu_id = entry.extra.split()[1]
-        assert otu_id.startswith("Otu")
-        taxonomy_entry = taxonomy.TaxonomyEntry(entry.identifier, taxonomy_map[otu_id])
-        factory.write_entry(taxonomy_entry)
-    factory.close()
+        print("Locating valid sequence IDs...")
+        otu_index_to_sequence_index = {
+            i: fasta_db.sequence_id_to_index(otu_to_sequence_id[header[i]])
+            for i in tqdm(range(3, len(header))) if fasta_db.contains_sequence_id(otu_to_sequence_id[header[i]])
+        }
+
+        lines = (line.strip().split('\t') for line in f)
+        factory = fasta.FastaMappingDbFactory(output_path / "sequences.mapping.fasta.db", fasta_db)
+        for row in tqdm(lines, desc="Writing sample mappings"):
+            sample_name = row[1]
+            mapping = factory.create_entry(sample_name)
+            for otu_index, sequence_index in otu_index_to_sequence_index.items():
+                if (abundance := int(row[otu_index])) == 0:
+                    continue
+                mapping.write_sequence_index(sequence_index, abundance)
+            factory.write_entry(mapping)
+        factory.close()
 
     # Copy metadata
     shutil.copy(metadata_path, output_path / f"{config.name}.metadata.csv")
+
+    # indices = [i for i in tqdm(range(3, len(header))) if index_db.contains_key(header[i])]
+    # fasta_ids = {i: index_db.key_to_fasta_id(header[i]) for i in tqdm(indices)}
+    # factory = sample.SampleMappingDbFactory(output_path / f"{config.name}.fasta.mapping.db")
+    # for row in tqdm(lines, desc="Writing sample mappings"):
+    #     sample_name = row[1]
+    #     sample_factory = sample.SampleMappingEntryFactory(sample_name, index_db)
+    #     for i in indices:
+    #         if (count := int(row[i])) == 0:
+    #             continue
+    #         fasta_id = fasta_ids[i]
+    #         sample_factory.add_entry(fasta_db[fasta_id], count)
+    #     factory.write_entry(sample_factory.build())
+    # factory.close()
+
+    # # Taxonomy
+    # taxonomy_data = pd.read_csv(taxonomy_path)
+    # taxonomy_map = {}
+    # for _, row in taxonomy_data.iterrows():
+    #     otu, *taxons = tuple(row)
+    #     label = taxonomy.join_taxonomy(taxons)
+    #     taxonomy_map[otu] = label
+    # factory = taxonomy.TaxonomyDbFactory(output_path / f"{config.name}.tax.tsv.db")
+    # for entry in tqdm(fasta_db, desc="Writing taxonomy"):
+    #     otu_id = entry.extra.split()[1]
+    #     assert otu_id.startswith("Otu")
+    #     taxonomy_entry = taxonomy.TaxonomyEntry(entry.identifier, taxonomy_map[otu_id])
+    #     factory.write_entry(taxonomy_entry)
+    # factory.close()
 
 
 if __name__ == "__main__":
     context = dcs.Context(main)
     _common.define_io_arguments(context.argument_parser)
-    _common.define_dataset_arguments(context.argument_parser, "SFD")
+    _common.define_dataset_arguments(context.argument_parser, "sfd")
     context.execute()
--- scripts/dataset/prepare_wetland.py
+++ scripts/dataset/prepare_wetland.py
@@ -18,56 +18,49 @@ def main(context: dcs.Context):
     output_path.mkdir(exist_ok=True)
 
     fasta_path = config.input_path / "P_A_201201_wet_libs1_8.trim.contigs.pcr.good.unique.good.filter.unique.precluster.pick.pick.agc.0.03.pick.0.03.abund.0.03.pick.fasta.new.fasta"
-    otu_list_path = config.input_path / "221205_cmfp.trim.contigs.pcr.good.unique.good.filter.unique.precluster.denovo.vsearch.asv.list"
-    otu_shared_path = config.input_path / "221205_cmfp.trim.contigs.pcr.good.unique.good.filter.unique.precluster.denovo.vsearch.asv.shared"
+    otu_list_path = config.input_path / "201201_wet_libs1_8.trim.contigs.pcr.good.unique.good.filter.unique.precluster.pick.pick.asv.list"
+    otu_shared_path = config.input_path / "201201_wet_libs1_8.trim.contigs.pcr.good.unique.good.filter.unique.precluster.pick.pick.asv.shared"
 
     # Create FASTA DB
-    factory = fasta.FastaDbFactory(output_path / f"{config.name}.fasta.db")
-    for entry in tqdm(map(clean_entry, fasta.entries(fasta_path))):
+    factory = fasta.FastaDbFactory(output_path / "sequences.fasta.db")
+    for entry in tqdm(map(clean_entry, fasta.entries(fasta_path)), desc="Writing FASTA DB"):
         if len(entry) < config.min_length:
             continue
         factory.write_entry(entry)
     factory.close()
-    fasta_db = fasta.FastaDb(output_path / f"{config.name}.fasta.db")
+    fasta_db = fasta.FastaDb(output_path / "sequences.fasta.db")
 
-    # Load OTU identifiers
+    # Create FASTA Index DB
     with open(otu_list_path) as f:
         keys = f.readline().strip().split('\t')
         values = f.readline().strip().split('\t')
-    otu_index = dict(zip(keys[2:], values[2:]))
+    otu_to_sequence_id = dict(zip(keys[2:], values[2:]))
 
-    # Create FASTA Index DB
-    factory = fasta.FastaIndexDbFactory(output_path / f"{config.name}.fasta.index.db")
-    for i, asv in enumerate(tqdm(otu_index)):
-        fasta_id = otu_index[asv]
-        if fasta_id not in fasta_db:
-            continue
-        factory.write_entry(fasta_db[fasta_id], key=asv)
-    factory.close()
-    index_db = fasta.FastaIndexDb(output_path / f"{config.name}.fasta.index.db")
-
+    # Create sample mappings
     with open(otu_shared_path) as f:
         header = f.readline().strip().split('\t')
-        lines = [line.strip().split('\t') for line in tqdm(f)]
 
-    indices = [i for i in tqdm(range(3, len(header))) if index_db.contains_key(header[i])]
-    fasta_ids = {i: index_db.key_to_fasta_id(header[i]) for i in tqdm(indices)}
+        print("Locating valid sequence IDs...")
+        otu_index_to_sequence_index = {
+            i: fasta_db.sequence_id_to_index(otu_to_sequence_id[header[i]])
+            for i in tqdm(range(3, len(header))) if fasta_db.contains_sequence_id(otu_to_sequence_id[header[i]])
+        }
 
-    factory = sample.SampleMappingDbFactory(output_path / f"{config.name}.fasta.mapping.db")
-    for row in tqdm(lines):
-        sample_name = row[1]
-        sample_factory = sample.SampleMappingEntryFactory(sample_name, index_db)
-        for i in indices:
-            if (count := int(row[i])) == 0:
-                continue
-            fasta_id = fasta_ids[i]
-            sample_factory.add_entry(fasta_db[fasta_id], count)
-        factory.write_entry(sample_factory.build())
-    factory.close()
+        lines = (line.strip().split('\t') for line in f)
+        factory = fasta.FastaMappingDbFactory(output_path / "sequences.mapping.fasta.db", fasta_db)
+        for row in tqdm(lines, desc="Writing sample mappings"):
+            sample_name = row[1]
+            mapping = factory.create_entry(sample_name)
+            for otu_index, sequence_index in otu_index_to_sequence_index.items():
+                if (abundance := int(row[otu_index])) == 0:
+                    continue
+                mapping.write_sequence_index(sequence_index, abundance)
+            factory.write_entry(mapping)
+        factory.close()
 
 
 if __name__ == "__main__":
     context = dcs.Context(main)
     _common.define_io_arguments(context.argument_parser)
-    _common.define_dataset_arguments(context.argument_parser, "Wetland")
+    _common.define_dataset_arguments(context.argument_parser, "wetland")
     context.execute()
