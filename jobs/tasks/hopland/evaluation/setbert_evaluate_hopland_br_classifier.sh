c245863e1687375eab0e28884c4ad0805a7ae750
David Ludwig <davidludwigii@gmail.com>
2024-02-12 12:26:01 -0600

    Hopland evaluation job script

--- jobs/tasks/hopland/evaluation/setbert_evaluate_hopland_br_classifier.sh
+++ jobs/tasks/hopland/evaluation/setbert_evaluate_hopland_br_classifier.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 #SBATCH --signal=INT@600
 
+source env.sh
+
+dataset_dir=${datasets[hopland]}
+
+# Copy data to scratch
+run_id=$$-$(date +%s%3N)
+if [ ! -z "${scratch_path}" ]; then
+    echo "Copying dataset to scratch path..."
+    mkdir -p "${scratch_path}/${run_id}/datasets/${dataset_dir}"
+    cp -vR "${datasets_path}/${dataset_dir}/sequences.fasta.db" "${scratch_path}/${run_id}/datasets/${dataset_dir}"
+    cp -vR "${datasets_path}/${dataset_dir}/sequences.fasta.mapping.db" "${scratch_path}/${run_id}/datasets/${dataset_dir}"
+    datasets_path="${scratch_path}/${run_id}/datasets"
+fi
+
+${command_prefix} ${python_tf} ./scripts/evaluation/setbert_evaluate_hopland_br_classifier.py \
+    --hopland-dataset-path "${datasets_path}/${dataset_dir}" \
+    --output-path "${log_path}/hopland" \
+    --model-artifact "${setbert_hopland_artifacts[topdown-silva-nr99-filtered-515f-806r]}" \
+    $@
+
+# Remove data from scratch
+if [ ! -z "${scratch_path}" ]; then
+    echo "Clearing scratch..."
+    rm -rf "${scratch_path}/${run_id}"
+fi
+echo "Done."
+
+
+#!/bin/bash
+#SBATCH --signal=INT@600
+
 if [ -z "${deepdna_env_loaded}" ]; then
     echo "deepdna environment not loaded. Please run 'source env.sh' first."
     exit 1
 fi
 
 ${command_prefix} ${python_tf} ./scripts/evaluation/setbert_evaluate_hopland_br_classifier.py \
-    --finetune-model-artifact $setbert_hopland_br_classifier \
-    --hopland-dataset-path $datasets_path/Hopland \
-    --output-path $log_path/setbert-evaluate-hopland-br-classifier \
+    --model-artifact $setbert_hopland_artifacts[topdown-silva-nr99-filtered-515f-806r] \
+    --hopland-dataset-path $datasets_path/hopland \
+    --output-path $log_path/results \
     $@
